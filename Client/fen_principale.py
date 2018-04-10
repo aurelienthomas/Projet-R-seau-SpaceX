fbc66ca6edbfabc0f06ad9c5051483f7d6717e63
Unknown <florian.combarel@orange.fr>
2018-04-10 14:58:37 +0200

    ajout fen principale interface

--- Client/fen_principale.py
+++ Client/fen_principale.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'projet_reseau.ui'
 #
-# Created by: PyQt4 UI code generator 4.11.4
+# Created by: PyQt4 UI code generator 4.12.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -22,56 +22,50 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_Frame(object):
-    def setupUi(self, Frame):
-        Frame.setObjectName(_fromUtf8("Frame"))
-        Frame.resize(503, 514)
-        Frame.setFrameShape(QtGui.QFrame.StyledPanel)
-        Frame.setFrameShadow(QtGui.QFrame.Raised)
-        self.edit_pseudo = QtGui.QLineEdit(Frame)
-        self.edit_pseudo.setGeometry(QtCore.QRect(82, 10, 291, 21))
-        self.edit_pseudo.setObjectName(_fromUtf8("edit_pseudo"))
-        self.label_pseudo = QtGui.QLabel(Frame)
-        self.label_pseudo.setGeometry(QtCore.QRect(10, 10, 61, 17))
-        self.label_pseudo.setObjectName(_fromUtf8("label_pseudo"))
-        self.connexion = QtGui.QPushButton(Frame)
-        self.connexion.setGeometry(QtCore.QRect(380, 10, 111, 21))
-        self.connexion.setObjectName(_fromUtf8("connexion"))
-        self.label_carte = QtGui.QLabel(Frame)
-        self.label_carte.setGeometry(QtCore.QRect(10, 40, 51, 17))
-        self.label_carte.setObjectName(_fromUtf8("label_carte"))
-        self.carte = QtGui.QTableWidget(Frame)
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(506, 514)
+        self.up = QtGui.QPushButton(Form)
+        self.up.setGeometry(QtCore.QRect(210, 400, 51, 41))
+        self.up.setText(_fromUtf8(""))
+        self.up.setObjectName(_fromUtf8("up"))
+        self.down = QtGui.QPushButton(Form)
+        self.down.setGeometry(QtCore.QRect(210, 450, 51, 41))
+        self.down.setText(_fromUtf8(""))
+        self.down.setObjectName(_fromUtf8("down"))
+        self.left = QtGui.QPushButton(Form)
+        self.left.setGeometry(QtCore.QRect(150, 430, 51, 41))
+        self.left.setText(_fromUtf8(""))
+        self.left.setObjectName(_fromUtf8("left"))
+        self.right = QtGui.QPushButton(Form)
+        self.right.setGeometry(QtCore.QRect(270, 430, 51, 41))
+        self.right.setText(_fromUtf8(""))
+        self.right.setObjectName(_fromUtf8("right"))
+        self.carte = QtGui.QTableWidget(Form)
         self.carte.setGeometry(QtCore.QRect(10, 60, 481, 321))
         self.carte.setObjectName(_fromUtf8("carte"))
         self.carte.setColumnCount(0)
         self.carte.setRowCount(0)
-        self.up = QtGui.QPushButton(Frame)
-        self.up.setGeometry(QtCore.QRect(220, 400, 51, 41))
-        self.up.setText(_fromUtf8(""))
-        self.up.setObjectName(_fromUtf8("up"))
-        self.up.setIcon(QIcon("Images/up.png"))
-        self.right = QtGui.QPushButton(Frame)
-        self.right.setGeometry(QtCore.QRect(280, 430, 51, 41))
-        self.right.setText(_fromUtf8(""))
-        self.right.setObjectName(_fromUtf8("right"))
-        self.up.setIcon(QIcon("Images/right.png"))
-        self.left = QtGui.QPushButton(Frame)
-        self.left.setGeometry(QtCore.QRect(160, 430, 51, 41))
-        self.left.setText(_fromUtf8(""))
-        self.left.setObjectName(_fromUtf8("left"))
-        self.up.setIcon(QIcon("Images/left.png"))
-        self.down = QtGui.QPushButton(Frame)
-        self.down.setGeometry(QtCore.QRect(220, 450, 51, 41))
-        self.down.setText(_fromUtf8(""))
-        self.down.setObjectName(_fromUtf8("down"))
-        self.up.setIcon(QIcon("Images/down.png"))
+        self.label_carte = QtGui.QLabel(Form)
+        self.label_carte.setGeometry(QtCore.QRect(10, 40, 51, 17))
+        self.label_carte.setObjectName(_fromUtf8("label_carte"))
+        self.label_pseudo = QtGui.QLabel(Form)
+        self.label_pseudo.setGeometry(QtCore.QRect(10, 10, 61, 17))
+        self.label_pseudo.setObjectName(_fromUtf8("label_pseudo"))
+        self.connexion = QtGui.QPushButton(Form)
+        self.connexion.setGeometry(QtCore.QRect(380, 10, 111, 21))
+        self.connexion.setObjectName(_fromUtf8("connexion"))
+        self.edit_pseudo = QtGui.QLineEdit(Form)
+        self.edit_pseudo.setGeometry(QtCore.QRect(82, 10, 291, 21))
+        self.edit_pseudo.setObjectName(_fromUtf8("edit_pseudo"))
 
-        self.retranslateUi(Frame)
-        QtCore.QMetaObject.connectSlotsByName(Frame)
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
 
-    def retranslateUi(self, Frame):
-        Frame.setWindowTitle(_translate("Frame", "Projet_Reseau", None))
-        self.label_pseudo.setText(_translate("Frame", "Pseudo :", None))
-        self.connexion.setText(_translate("Frame", "Connexion", None))
-        self.label_carte.setText(_translate("Frame", "Carte :", None))
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "Projet_Reseau", None))
+        self.label_carte.setText(_translate("Form", "Carte :", None))
+        self.label_pseudo.setText(_translate("Form", "Pseudo :", None))
+        self.connexion.setText(_translate("Form", "Connexion", None))
 
--- Client/projet_reseau.py
+++ Client/projet_reseau.py
@@ -1,7 +1,7 @@
 from PyQt4 import QtGui
-from fen_principale import Ui_Frame
+from fen_principale import Ui_Form
 
-class MajQt(QtGui.QFrame, Ui_Frame):
+class MajQt(QtGui.QWidget, Ui_Form):
 	def __init__(self, parent=None):
 		QtGui.QWidget.__init__(self, parent)
 		self.setupUi(self)
@@ -11,4 +11,5 @@ if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
 	win = MajQt()
 	win.show
+	print("Application lancée !")
 	sys.exit(app.exec_())
