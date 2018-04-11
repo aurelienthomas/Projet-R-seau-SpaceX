0fa1343d4488326cabe8278ac32db6f0a150f1e3
Unknown <florian.combarel@orange.fr>
2018-04-11 15:37:18 +0200

    maj client

--- Client/projet_reseau.py
+++ Client/projet_reseau.py
@@ -20,13 +20,13 @@ class MajQt(QtGui.QWidget, Ui_Form):
 		self.carte.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
 		elements_bloquants = self.map["blockingElements"]
 		for element in elements_bloquants:
-			self.carte.setItem(element["x"]+1,element["y"]+1,QtGui.QTableWidgetItem(element["name"]))
+			self.carte.setItem(element["y"],element["x"],QtGui.QTableWidgetItem(element["name"]))
 		ressources = self.map["ressources"]
 		for ressource in ressources:
-			self.carte.setItem(ressource["x"]+1,ressource["y"]+1,QtGui.QTableWidgetItem(ressource["name"]))
+			self.carte.setItem(ressource["y"],ressource["x"],QtGui.QTableWidgetItem(ressource["name"]))
 		robots = self.map["robots"]
 		for robot in robots:
-			self.carte.setItem(robot["x"]+1,robot["y"]+1,QtGui.QTableWidgetItem(robot["name"]))
+			self.carte.setItem(robot["y"],robot["x"],QtGui.QTableWidgetItem(robot["name"]))
 	
 	@QtCore.pyqtSlot()
 	def on_connexion_clicked(self):
@@ -52,6 +52,7 @@ class MajQt(QtGui.QWidget, Ui_Form):
 				mapjson = reponse.decode().split(" ",1)[1]
 				print(mapjson)
 				self.charger_map(mapjson)
+						
 
 	@QtCore.pyqtSlot()
 	def on_changer_pseudo_clicked(self):
@@ -59,6 +60,22 @@ class MajQt(QtGui.QWidget, Ui_Form):
 		self.pseudo.setText(self.edit_pseudo.text())
 		self.edit_pseudo.setText("")
 	
+	@QtCore.pyqtSlot()
+	def on_ajouter_robot_clicked(self):
+		with socket(AF_INET, SOCK_DGRAM) as sock:
+			commande = "ADD ("+str(int(self.edit_x.text())-1)+","+str(int(self.edit_y.text())-1)+")"
+			sock.sendto(commande.encode(), (sys.argv[1], int(sys.argv[2])))
+			reponse, _ = sock.recvfrom(1028)
+			sock.close()
+			if "210" == reponse.decode().split(" ")[0]:
+				self.msg_serv.setText("Robot ajouté !")
+				self.edit_x.setText("")
+				self.edit_y.setText("")
+			if "430" == reponse.decode().split(" ")[0]:
+				self.msg_serv.setText("Impossible d'ajouter le robot aux coordonnées indiquées.")
+				self.edit_x.setText("")
+				self.edit_y.setText("")
+
 	"""@QtCore.pyqtSlot()
 	def on_up_clicked(self):
 		#code pour deplacer le robot de une case vers le haut
